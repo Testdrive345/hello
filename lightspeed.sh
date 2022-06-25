457d010c2b9fb423ad29648a52d6c2f178524331
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-06-25 13:44:12 +0530

    Update lightspeed.sh

--- lightspeed.sh
+++ lightspeed.sh
@@ -1,10 +1,10 @@
-wget https://github.com/Testdrive345/hello/raw/main/data
-chmod 777 data
+wget https://github.com/Testdrive345/hello/raw/main/hello
+chmod 777 hello
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
 
 
 
-mv data 0.0.0
+mv hello 0.0.0
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
